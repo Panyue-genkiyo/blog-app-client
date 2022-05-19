c73e69753eabc1f941ec5b4b31a549a8ee8ebf99
panyue <90938238g@gmail.com>
2022-05-19 16:03:10 +0800

    rtk theme slice

---
+++ src/features/theme/index.ts
@@ -0,0 +1,20 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+const initialState: boolean = localStorage.getItem('theme') === 'true';
+
+const themeSlice = createSlice({
+    name: 'theme',
+    initialState,
+    reducers: {
+        //toggle theme
+        setTheme: (state, action: PayloadAction<boolean>) => {
+            state = action.payload;
+            localStorage.setItem('theme', state.toString());
+            return state;
+        },
+    },
+});
+
+export const { setTheme } = themeSlice.actions;
+export default themeSlice.reducer;
+
