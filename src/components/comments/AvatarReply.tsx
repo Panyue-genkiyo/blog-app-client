9b6c3587d88040332280bf0b06d8dd82e42d0992
panyue <90938238g@gmail.com>
2022-05-19 16:40:48 +0800

    avatar reply组件

--- src/components/comments/AvatarReply.tsx
+++ src/components/comments/AvatarReply.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-import { IUser } from '../../types/Typescript'
+import { IUser } from '../../types/globalTypes'
 
 interface IProps {
     user: IUser
