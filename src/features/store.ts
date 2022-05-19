788083c59c891057b185049523065c19a04d7461
panyue <90938238g@gmail.com>
2022-05-19 16:05:47 +0800

    rtk store

---
+++ src/features/store.ts
@@ -0,0 +1,23 @@
+import { configureStore } from "@reduxjs/toolkit";
+import themeReducer from "./theme";
+import authReducer from "./auth";
+import alertReducer from "./alert";
+import userLocationReducer from "./userlocation";
+import picReducer from './picFile';
+import socketReducer from "./socket";
+
+export const store = configureStore({
+    reducer: {
+        theme: themeReducer,
+        auth: authReducer,
+        alert: alertReducer,
+        userLocation: userLocationReducer,
+        pic: picReducer,
+        socket: socketReducer,
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+});
+
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
