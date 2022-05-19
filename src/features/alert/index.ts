45c75d9a5c213113d638b8952965e59693e91f55
panyue <90938238g@gmail.com>
2022-05-19 16:42:56 +0800

    rtk alert slice

--- src/features/alert/index.ts
+++ src/features/alert/index.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import {IAlert} from "../../types/Typescript";
+import {IAlert} from "../../types/globalTypes";
 
 const initialState: IAlert = {};
 
